9c05ff7bbafddc78ab6a8f8d41208336cc9c6bb8
Wanderlee Max <wanderleedev@gmail.com>
2023-05-20 23:57:08 -0500

    implementando js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,27 @@
-//botones
-const btnEncryp = document.getElementById('btnEncryp');
-const btnDecrypt = document.getElementById('btnDecrypt');
-const btnCopy = document.getElementById('btnCopy');
-//caja de entrada
-const boxMessage = document.getElementById('boxMessage');
-//caja de salida
-const outletInfo = document.querySelector('.outletBox__info');
-const outletMessage = document.querySelector('.outletBox__message');
-const image = document.querySelector('.secondSection__img');
+//Selección de elementos
+const boxMessage = document.querySelector('#boxMessage'); //caja de texto
+const encrypt = document.querySelector('#btnEncrypt'); //btn de encriptación
+
+
+//evento para capturar el mensaje de la caja de texto
+const eventListenerBox = (target)=>{
+
+  const message = target.value; //captura del objetivo
+  
+  //verifica que el contenido no este vació
+  if (!message) {
+    alert('Contenido vació');
+    return;
+  }
+
+
+}
+
+//const replaceValues = ()=>{};
+encrypt.addEventListener('click',()=> eventListenerBox(boxMessage));
+
+
+
 
 //llaves de encriptación
 const key1 = "e";//enter
@@ -17,26 +31,3 @@ const key4 = "o";//ober
 const key5 = "u";//ufat
 let keys = [key1, key2, key3, key4, key5].join("|");
 let regExp = new RegExp(keys,"g");
-
-//copiar mensaje al portapapeles
-function copiar2() {
-  outletInfo.select;
-  document.execCommand('copy');
-}
-
-//función para imprimir mensaje
-function encriptar() {
-  let tempMessage = outletMessage.value;
-  console.log(tempMessage.value);
-  image.style.display = 'none';
-  outletInfo.style.display = 'none';
-  
-  outletMessage.textContent = "ahsadashdvsahcadkhfkajjbsbjxjsjsbasbkcaskbcbascbaskbcabcas";
-}
-
-btnEncryp.addEventListener('click',function () {
-  encriptar();
-});
-btnCopy.addEventListener("click",function () {
-  copiar2(boxMessage);
-});
